9efc6c1df1a8392ccc040e379eb3171e2875c527
algins <igors.algins@gmail.com>
2024-06-13 20:51:53 +0300

    chore: update plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,11 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    application
     jacoco
     id("checkstyle")
     id("io.freefair.lombok") version "8.6"
     id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
